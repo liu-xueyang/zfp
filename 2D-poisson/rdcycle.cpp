dbb3bd2285caceb16959a4067b00f37d2af36fc5
Xueyang Liu <marliu@umich.edu>
2023-07-14 17:25:52 -0700

    include CSR read cycle

--- 2D-poisson/rdcycle.cpp
+++ 2D-poisson/rdcycle.cpp
@@ -16,19 +16,31 @@ static __inline__ uint64_t rdtsc(void)
         __asm__ volatile("rdcycle %0" : "=r"(cycles));
         return cycles;
 }
+
+// #define read_csr(reg) ({ unsigned long __tmp; 
+// asm volatile ("csrr %0, " #reg : "=r"(__tmp)); 
+// __tmp; })
+// #define CSR_CYCLE 0xc00
+// #define CSR_TIME 0xc01
+
+static __inline__ uint64_t rdcsr_cycle(void)
+{
+    return read_csr(cycle);
+}
 #endif
 
 int main(int argc, char* argv[])
 {
-    uint64_t c1 = rdtsc();
-    printf("start cycle is %llu\n", c1);
-    
+    uint64_t c1 = rdtsc(), c1_csr = rdcsr_cycle();
+    printf("start rdcycle is %llu, csr cycle is %llu\n", c1, c1_csr);
+
     for (int i=0; i<10; i++)
         printf("%d ", i*5);
     printf("\n");
 
-    uint64_t c2 = rdtsc();
-    printf("end cycle is %llu\n", c2);
+    uint64_t c2 = rdtsc(), c2_csr = rdcsr_cycle();
+    printf("end rdcycle is %llu, csr cycle is %llu\n", c2, c2_csr);
     printf("cycle difference is %llu\n", c2-c1);
+    printf("csr cycle difference is %llu\n", c2_csr-c1_csr);
 
 }
